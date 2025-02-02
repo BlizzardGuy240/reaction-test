ebae0c5a7008c06928f8f46293d84dc0a3674c32
BlizzardGuy240 <122892736+BlizzardGuy240@users.noreply.github.com>
2025-02-02 19:20:34 +0400

    Update reaction_test.py
    Cleaning stuff up

--- reaction_test.py
+++ reaction_test.py
@@ -8,7 +8,7 @@ l = []
 pygame.init()
 screen = pygame.display.set_mode((650, 700))
 
-pygame.draw.rect(screen, (255, 255, 255), (0, 0, 650, 700)) #1000, 600
+pygame.draw.rect(screen, (255, 255, 255), (0, 0, 650, 700))
 pygame.display.update()
 pygame.display.set_caption("Reaction Test")
 
@@ -31,8 +31,7 @@ def lights():
     global start
     start = t.perf_counter()
     global turn
-    turn += 1
-    
+    turn += 1   
 
     
 def draw_board():
@@ -43,13 +42,9 @@ def draw_board():
             pygame.draw.circle(screen, (0, 0, 0), (125+i, j), 70)
     pygame.display.update()
 
-
-
 draw_board()
 l1 = []
 l2 = []
-
-
 turn = 0
 
 while True:
@@ -63,13 +58,7 @@ while True:
             pygame.draw.rect(screen, (255, 255, 255), (0, 500, 1000, 1000))
             lights()
             pygame.event.clear()
-            l1 += [1]
-            # if event.type == pygame.MOUSEBUTTONDOWN and turn == 1 and len(l1) == 1:
-            #     print("jumpstart")
-            #     pygame.event.clear()
-            #     del l1[0]
-            #     break
-            
+            l1 += [1]         
             
         elif event.type == pygame.MOUSEBUTTONDOWN and turn == 1 and len(l1) == 1:
             del l1[0]
@@ -82,39 +71,5 @@ while True:
             pygame.display.update()
             turn -= 1
             l2 += ["{:.2f}".format(a*1000)]
-
-
-
-
-            
-
-
-            # for i in range(750, 950):
-            #     for j in range(50, 150):
-            #         if event.pos == (i, j) and pxarray[i, j] == screen.map_rgb(255, 0, 0):
-            #             lights()
-            #             pygame.event.clear()
-            #             if pxarray[125, 150] == screen.map_rgb(0, 0, 0):
-            #                 end = t.perf_counter()
-            #                 print(end - start)
-
-                            
-                            
-                        
-                    
-            # if event.type == pygame.MOUSEBUTTONDOWN and turn == 1:
-            #     reset()
-            #     turn = turn % 2
-                        
-                    
-                        
-                        
-            # if pxarray[400+125,300] == screen.map_rgb(255, 0, 0):
-            #     pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)
-            #     if event.type == pygame.MOUSEBUTTONDOWN:
-            #         draw_board()
-            # print(event.type)
-            # pass
-
 
         
